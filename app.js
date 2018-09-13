6de418a4462bafa8fe4792bde54801c560041293
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-13 19:23:25 +0200

    create todos

--- app.js
+++ app.js
@@ -1,33 +1,90 @@
+
 function updateCounters() {
-    // Total number of todos
-    var totalCount = document.getElementById('total-count');
-    var totalTodos = document.getElementsByClassName("todo").length;
-    totalCount.innerHTML = totalTodos;
+
+// select the element containing the count
+var totalCount = document.getElementById('total-count');
+// // count the number of todo's by their class name
+var totalTodos = document.getElementsByClassName("todo").length;
+// update the HTML inside the counter element
+
+totalCount.innerHTML = totalTodos;
+
+// Total number of completed todos
+
+var completedCount = document.getElementById('completed-count');
+var completedTodos = document.getElementsByClassName("completed").length;
+completedCount.innerHTML = completedTodos;
 
 
-    // Total number of completed todos
-    var completedCount = document.getElementById('completed-count');
-    var completedTodos = document.getElementsByClassName("completed").length;
-    completedCount.innerHTML = completedTodos;
+// Total number of uncompleted todos
+var todoCount = document.getElementById('todo-count');
+var uncompletedTodos = totalTodos - completedTodos;
+todoCount.innerHTML = uncompletedTodos;
 
-    // Total number of uncompleted todos
-    var todoCount = document.getElementById('todo-count');
-    var uncompletedTodos = totalTodos - completedTodos;
-    todoCount.innerHTML = uncompletedTodos;
-  }
-  
-  updateCounters();
+}
 
-  function toggleDone() {
+function toggleDone() {
     var checkbox = this;
-  
     // check the checked status of the checkbox
     if (checkbox.checked) {
-      // the "completed" class is set on the parent element, the <li>
-      checkbox.parentElement.className = "todo completed";
+    // the "completed" class is set on the parent element, the <li>
+        checkbox.parentElement.className = "todo completed";
+
     } else {
-      checkbox.parentElement.className = "todo";
+        checkbox.parentElement.className = "todo";
     }
-  
     updateCounters();
+}
+
+function submitTodo() {
+    var inputField = document.getElementById("new-todo");
+    var newTodoTitle = inputField.value;
+    createTodo(newTodoTitle);
+
+    // reset the value of the inputField to make it empty and
+    // ready to create new todos
+    inputField.value = null;
+    updateCounters();
+}
+
+
+function createTodo(title) {
+    // create a list item
+    var listItem = document.createElement("li");
+    listItem.className = "todo";
+  
+    // create a checkbox and add it to the list item
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "todo-" + nextTodoId();
+    checkbox.checked = false;
+    // assign the toggleDone function on the checkbox's onchange event
+    checkbox.onchange = toggleDone.bind(checkbox);
+    listItem.appendChild(checkbox);
+  
+    // create some whitespace to put between the checkbox and the label
+    var space = document.createTextNode(" ");
+    listItem.appendChild(space);
+  
+    // create a label that holds the title and add it to the list item
+    var label = document.createElement("label");
+    label.htmlFor = checkbox.id;
+    label.innerHTML = title;
+    listItem.appendChild(label);
+  
+    // add the list item with the checkbox, the whitespace and the label to
+    // the list
+    var list = document.getElementById("todolist");
+    list.appendChild(listItem);
   }
+  
+  // Every todo has it's own id so we can add that to the corresponding label's
+  // "for" attribute to make sure that when we click the label, the checkbox
+  // toggles
+  function nextTodoId() {
+    return document.getElementsByClassName("todo").length + 1;
+  }
+
+
+
+// updateCounters();
